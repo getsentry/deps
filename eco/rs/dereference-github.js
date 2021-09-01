1df2f1dd1e3aabbbd634984cf13cc9929d40de07
Chad Whitacre <chadwhitacre@sentry.io>
2021-09-01 14:45:25 -0400

    Implement GitHub dereferencing for Rust (easy)

--- eco/rs/dereference-github.js
+++ eco/rs/dereference-github.js
@@ -1,4 +1,10 @@
 const deps = require('./deps.json');
 const lib = require('../../lib.js');
 
-console.log(lib.github);
+for ([org_repo, count] of Object.entries(deps)) {
+  let [org, repo] = org_repo.split('/');
+  let count = deps[org_repo];
+  lib.record(org, repo, count)
+}
+
+lib.dump();
--- lib.js
+++ lib.js
@@ -1,4 +1,15 @@
 const github = require('./github.json');
+const fs = require('fs');
 
+function record(org, repo, count) {
+  if (github[org] === undefined) {
+    github[org] = {}
+  }
+  github[org][repo] = count;
+}
 
-module.exports = {github};
+function dump() {
+  fs.writeFileSync('github.json', JSON.stringify(github));
+}
+
+module.exports = {record, dump, github};
