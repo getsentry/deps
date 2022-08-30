e0afd3b6cf89a4cf7ca7825fbdf2f9eaa5b194d2
Chad Whitacre <chadwhitacre@sentry.io>
2022-08-30 10:37:32 -0400

    Add a little comment about Rust dereferencing

--- eco/rs/dereference-github.js
+++ eco/rs/dereference-github.js
@@ -1,6 +1,7 @@
 const deps = require('./deps.json');
 const lib = require(`${__dirname}/../../lib`);
 
+// For Rust we get GitHub links directly, no need to hit another API.
 for ([org_repo, sources] of Object.entries(deps)) {
   let [org, repo] = org_repo.split('/');
   lib.record('rs', org, repo, sources)
