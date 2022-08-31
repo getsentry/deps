fcb05efc776f53bdf97733ff24dfd9f6b2b42d54
Chad Whitacre <chadwhitacre@sentry.io>
2022-08-31 12:57:13 -0400

    Add extract.js for PHP

---
+++ eco/php/extract.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const process = require('process')
+
+process.chdir(__dirname)
+
+deps = {}
+
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('deps-files')
+});
+
+lineReader.on('line', function (line) {
+  var filepath = `../../repos/ours/${line}`;
+  var pj = require(filepath);
+
+  for (var key of ['require', 'require-dev']) {
+    if (pj[key]) {
+      for (const [name, _] of Object.entries(pj[key])) {
+        if (deps[name] === undefined) {
+          deps[name] = [];
+        }
+        deps[name].push(line);
+      }
+    }
+  }
+});
+
+lineReader.once('close', () => {
+  fs.writeFileSync('deps.json', JSON.stringify(deps, null, 2))
+});
